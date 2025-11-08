315912e0cefa99b266059d57a2bd6d9be480c244
S-Iwata <iwata@ichiken-engineering.com>
2025-11-09 03:00:22 +0900

    venv移行

--- setup.bat
+++ setup.bat
@@ -1,6 +1,15 @@
 @echo off
-echo Installing Libraries...
-pip install PySimpleGUI deepl
+
+rem Setup Python virtual environment
+set "SCRIPT_DIR=%~dp0"
+set "VENV_DIR=%SCRIPT_DIR%venv"
+
+echo Building the virtual environment...
+if not exist "%VENV_DIR%" (
+    python -m venv "%VENV_DIR%"
+)
+echo Installing Libraries..."%VENV_DIR%\Scripts\python.exe" -m pip install --upgrade pip
+"%VENV_DIR%\Scripts\pip.exe" install -r "%SCRIPT_DIR%requirements.txt"
 echo Generating setting file...
 if not exist settings.py (
     echo DEEPL_API_KEY = "xxxxxxxx-xxxx-......:fx" > settings.py
